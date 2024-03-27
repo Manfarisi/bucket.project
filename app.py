826667de88ea5caaba9cc60ec8cfa69d3713d65b
Manfarisi <kingmansalman@gmail.com>
2024-03-27 16:04:51 +0700

    Add new file

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://kingmansalman:RyQX7bMSctOnaHgz@cluster0.w0rzxwl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+db = client.dbsparta
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route("/bucket", methods=["POST"])
+def bucket_post():
+    bucket_receive = request.form["bucket_give"]
+    count = db.bucket.count_documents({})
+    num = count + 1
+    doc = {
+        'num':num,
+        'bucket': bucket_receive,
+        'done':0
+    }
+    db.bucket.insert_one(doc)
+    return jsonify({'msg':'data saved!'})
+
+
+@app.route("/bucket/done", methods=["POST"])
+def bucket_done():
+    num_receive = request.form["num_give"]
+    db.bucket.update_one(
+        {'num': int(num_receive)},
+        {'$set': {'done': 1}}
+    )
+    return jsonify({'msg': 'Update done!'})
+
+@app.route("/bucket/delete", methods=["DELETE"])
+def bucket_delete():
+    num_delete = request.form["num_delete"]
+    result = db.bucket.delete_one({'num': int(num_delete)})
+    if result.deleted_count == 1:
+        return jsonify({'msg': 'Entry deleted successfully!!'})
+    else:
+        return jsonify({'msg': 'Entry not found or could not be deleted!!'})
+
+
+@app.route("/bucket", methods=["GET"])
+def bucket_get():
+    buckets_list = list(db.bucket.find({},{'_id':False}))
+    return jsonify({'buckets':buckets_list})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
